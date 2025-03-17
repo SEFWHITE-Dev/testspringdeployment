4494781fa1c4fcaa671c047d6ed9dd7f3da4cd79
WhiteNoise01 <153906600+SEFWHITE-Dev@users.noreply.github.com>
2025-03-17 09:53:55 +0900

    Update PostController.java

--- src/main/java/dev/danvega/blog/controller/PostController.java
+++ src/main/java/dev/danvega/blog/controller/PostController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @CrossOrigin(origins = "*") // Allow all origins (or specify your frontend URL)
 @RestController
